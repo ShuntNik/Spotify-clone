dd07088b666151a5bc8cb9e5b440eb78b9a0f386
ShuntNik <nikhilks1908@gmail.com>
2023-01-16 12:31:42 +0530

    play pause commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("welcome to spotify");
+let songindex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar')
+
+
+let songs =[
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "House of Memories", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+]
+
+//Hnadle p-lay pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    }
+})
+
+//Listen to events
