96d9302093140bdd3377fb5cf19082e1c93f7839
ShuntNik <nikhilks1908@gmail.com>
2023-01-16 13:01:48 +0530

    seekbar update commit

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ console.log("welcome to spotify");
 let songindex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar')
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 
 let songs =[
@@ -21,7 +22,23 @@ masterPlay.addEventListener('click', ()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
     }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+
 })
 
 //Listen to events
+audioElement.addEventListener('timeupdate', ()=>{
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value/100*audioElement.duration;
+})
