abc828597303ca2592ef4ab4b1e73a8a28ff65f3
ShuntNik <nikhilks1908@gmail.com>
2023-01-17 16:58:51 +0530

    song list commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('song-item'));
 
 
 let songs =[
@@ -13,8 +14,14 @@ let songs =[
     {songName: "Light em up", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Bad habbits", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Teeth", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Run BTS", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"}
+    {songName: "Run BTS", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
 ]
+console.log("hello");
+
+songItems.forEach((element,i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName
+});
 
 //Hnadle p-lay pause click
 masterPlay.addEventListener('click', ()=>{
@@ -41,4 +48,21 @@ audioElement.addEventListener('timeupdate', ()=>{
 
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = myProgressBar.value/100*audioElement.duration;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+        element.classList.remove("fa-circle-pause");
+        element.classList.add("fa-circle-play");
+    })
+}
+
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        console.log(e.target)
+        makeAllPlays();
+        e.target.classList.remove("fa-circle-play");
+        e.target.classList.add("fa-circle-pause");
+    })
 })
